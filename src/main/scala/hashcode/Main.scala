469d548a4d2dcdd8fe0f0b729ece4d64df947888
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2017-02-23 20:08:55 +0100

    Lectura de datos, sin probar ni ná

--- src/main/scala/hashcode/Main.scala
+++ src/main/scala/hashcode/Main.scala
@@ -1,24 +1,77 @@
 package hashcode
 
-/**
- * Created by alvaro on 11/12/14.
- */
-object Main extends App{
+import java.util.Scanner
 
-  def isPrime( p: Int ) = (2 to p/2).find( p % _ == 0 ).isEmpty
 
-  def isPalindrome( l: Int ) = l.toString == l.toString.reverse
+object Main extends App {
 
-  def haveSomeDigits(digit: Int, asMuchAsOrMore: Int )( number: Int) = {
-    val digitAsChar = digit.toString.charAt(0)
-    number.toString.count( _ == digitAsChar ) >= asMuchAsOrMore
+
+  /*
+  5 2 4 3 100
+  50 50 80 30 110
+  1000 3
+  0 100
+  2 200
+  1 300
+  500 0
+  3 0 1500
+  0 1 1000
+  4 0 500
+  1 0 1000
+
+  5 videos, 2 endpoints, 4 request descriptions, 3 caches 100MB each.
+    Videos 0, 1, 2, 3, 4 have sizes 50MB, 50MB, 80MB, 30MB, 110MB.
+  Endpoint 0 has 1000ms datacenter latency and is connected to 3 caches:
+    The latency (of endpoint 0) to cache 0 is 100ms.
+  The latency (of endpoint 0) to cache 2 is 200ms.
+    The latency (of endpoint 0) to cache 1 is 200ms.
+    Endpoint 1 has 500ms datacenter latency and is not connected to a cache.
+  1500 requests for video 3 coming from endpoint 0.
+  1000 requests for video 0 coming from endpoint 1.
+  500 requests for video 4 coming from endpoint 0.
+  1000 requests for video 1 coming from endpoint 0.
+  */
+  def parse(in: Scanner) = {
+    val Array(v, e, r, c, x) = in.nextLine().split(" ").map(_.toInt)
+
+    // VIDEOS
+    val videos = for (_ <- 0 to v) yield new Video( in.nextInt() )
+
+    // ENDPOINTS
+    val endpoints = for (endpoint <- 0 to e) yield {
+      val datacenterLatency = in.nextInt
+      val numberOfCaches = in.nextInt
+
+      val latenciesToCaches = (for (cache <- 0 to numberOfCaches) yield {
+        val cacheIndex = in.nextInt
+        val latencyEndpointToCache = in.nextInt
+        (cacheIndex, latencyEndpointToCache)
+      }).toMap
+
+      Endpoint(datacenterLatency, latenciesToCaches)
+    }
+
+    // REQUEST DESCRIPTORS
+    val requestDescriptors = for( descriptor <- 0 to r) yield{
+      val (requests, video, endpoint) = ( in.nextInt, in.nextInt, in.nextInt )
+      RequestDescriptor( requests, video, endpoint )
+    }
+
+    Data( x, videos, endpoints, requestDescriptors )
   }
 
-  val haveSomeThrees = haveSomeDigits( 3, 3 )_
+  case class RequestDescriptor( request: Int, video: Int, endPoint: Int )
 
-  for( l <- 2 to 1000000 if isPalindrome(l) && haveSomeThrees(l) && isPrime(l) ){
-    println( l )
+  case class Video(size: Int)
+
+  case class Endpoint(datacenterLantency: Int, latenciesToCaches: Map[Int, Int]) {
+    def latencyToCache(index: Int) = latenciesToCaches(index)
   }
 
-  println( "Done")
+  case class Data( cacheSize: Int,
+                   videos: IndexedSeq[Video],
+                   endpoints: IndexedSeq[Endpoint],
+                   requestDescriptors: IndexedSeq[RequestDescriptor] )
+
+
 }
